6d8babed5ec3b825a8ca27e89df30fa9e2bde21a
ChiChien <p10588chain@gmail.com>
2023-12-05 16:27:24 +0800

    update Startup middleware setting

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace SS.Core{
     public class Startup{
@@ -14,9 +17,37 @@ namespace SS.Core{
 
         public void ConfigureServices(IServiceCollection services){
 
+            services.AddApiVersioning(option =>{
+                option.DefaultApiVersion = new ApiVersion(1, 0);
+                option.AssumeDefaultVersionWhenUnspecified = true;
+                option.ReportApiVersions = true;
+
+            });
+
+            services.AddControllers();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env ){
+            
+            if(env.IsDevelopment()){
+                app.UseDeveloperExceptionPage();
+            }else{
+                app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithReExecute("Home/Error/{0}");
+                app.UseHsts();
+            }
+            // Middleware setting
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints=>{
+                endpoints.MapControllers();
+                endpoints.MapGet("/", async context => {
+                    await context.Response.WriteAsync("Hello, World!");
+                });
+            });
 
         }
     }
